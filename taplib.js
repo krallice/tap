c100dcd22fe9be082fa4237704ffd4985992c6ce
root <root@noclab.com.au>
2016-01-04 15:01:55 +1100

    Refactored JSON Load Routine

--- taplib.js
+++ taplib.js
@@ -1,16 +1,23 @@
+// When Document is Ready:
 $(document).ready(function(){
+
+  // Load our JSON Config:
+  loadConfig();
+
+});
+
+function loadConfig() {
 
   $.getJSON("config.json", function(json) {
 
+    // Populate our BPM Selector:
     $.each(json.tempo, function(entryIndex, entry) {
-
       $('#bpmSelector').append($('<option>', {
         value: this.bpm,
         text: this.bpm + " bpm"
       }));
-
     });
 
   });
 
-});
+}
