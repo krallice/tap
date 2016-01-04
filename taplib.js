c56370a609d6da9d026aadc7e6aff1ff6b2e1205
root <root@noclab.com.au>
2016-01-04 14:40:47 +1100

    Now autopopulating #bpmSelector with BPMs from JSON source

--- taplib.js
+++ taplib.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+  $.getJSON("config.json", function(json) {
+
+    $.each(json.tempo, function(entryIndex, entry) {
+
+      $('#bpmSelector').append($('<option>', {
+        value: this.bpm,
+        text: this.bpm + " bpm"
+      }));
+
+    });
+
+  });
+
 });
