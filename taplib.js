7d0d1349c4b944eb76fb630422541bc8d4d03bf0
root <root@noclab.com.au>
2016-01-05 11:50:16 +1100

    Basic crotchet metronome function completed with sfx

--- taplib.js
+++ taplib.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
   // Load our JSON Config:
   loadConfig();
 
+  // Delay Value between tick()s:
+  var metronomeDelay=3000;
+
   // Setup our Start Button:
   var metronomeEnabled=0;
   var metronomeHandle=0;
   $("#startButton").click(function() {
     metronomeEnabled = 1 - metronomeEnabled;
     if ( metronomeEnabled == 1 ) {
+      metronomeDelay=calculateDelay($("#bpmSelector").val());
       metronomeTick();
-      metronomeHandle = setInterval(metronomeTick, 3000);
+      metronomeHandle = setInterval(metronomeTick, metronomeDelay);
     } else {
       clearInterval(metronomeHandle);
     }
@@ -18,12 +22,17 @@ $(document).ready(function(){
 
 });
 
+// Only calculates based on crotchet notes to start with:
+function calculateDelay(bpm) {
+  var delay = 60000 / bpm;
+  return delay;
+}
+
 function metronomeTick() {
-  alert("tick");
+  $("#click1").trigger('play');
 }
 
 function loadConfig() {
-
   $.getJSON("config.json", function(json) {
 
     // Populate our BPM Selector:
@@ -35,5 +44,4 @@ function loadConfig() {
     });
 
   });
-
 }
